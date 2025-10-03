bf585110192c4d7966d722308d0a95a410d23d9b
Amey Patil <amey35195@gmail.com>
2025-10-03 16:16:46 +0000

    noo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,12 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
 export function Logo({ className }: { className?: string }) {
   return (
-    <div className={className}>
-      <img
-        src="/logo.png"
-        alt="Krisha Enterprise Logo"
-        style={{ width: '100%', height: '100%', objectFit: 'contain' }}
-      />
-    </div>
+    <img
+      src="/logo.png"
+      alt="Krisha Enterprise Logo"
+      className={className}
+    />
   );
 }
