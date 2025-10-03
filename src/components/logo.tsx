4bae46fd8d28cb425421993693b228ac1c518914
Amey Patil <amey35195@gmail.com>
2025-10-03 16:16:11 +0000

    the logo is still not visible

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 export function Logo({ className }: { className?: string }) {
   return (
-    <img
-      src="/logo.png"
-      alt="Krisha Enterprise Logo"
-      className={className}
-    />
+    <div className={className}>
+      <img
+        src="/logo.png"
+        alt="Krisha Enterprise Logo"
+        style={{ width: '100%', height: '100%', objectFit: 'contain' }}
+      />
+    </div>
   );
 }
